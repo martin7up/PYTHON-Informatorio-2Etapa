234b3054e5f0bfdd4271d74bbd22e6b22e294e41
martin7up <ing.csmj@gmail.com>
2024-07-25 18:03:22 -0300

    Practica de ciclos, condicionales, funciones y estructuras.

---
+++ clase25072024.py
@@ -0,0 +1,43 @@
+''''Obtener el mayor de tres numero ingresados por teclado.'''
+# num1 = int( input('Ingrese un primer numero >>> '))
+# num2 = int( input('Ingrese un segundo numero >>> '))
+# num3 = int( input('Ingrese un tercer numero >>> '))
+
+# if num1 > num2 and num1 > num3 :
+#     print(f'El orden es : {num1}')
+# elif num2 > num1 and num2 > num3 :
+#     print(f'El valor {num2} es el mayor de todos.')
+# elif num3 > num1 and num3 > num2 :
+#     print(f'El valor {num3} es el mayor de todos.')
+# else :
+#     print(f'Los tres numeros son iguales.')
+
+'''Ordenar de menor a mayor tres numero ingresados por teclado'''
+#Solo a modo de mostrar el uso de condicionales; mejor emplear algoritmos de ordenamiento p/estructuras.
+num1 = int( input('Ingrese un primer numero >>> '))
+num2 = int( input('Ingrese un segundo numero >>> '))
+num3 = int( input('Ingrese un tercer numero >>> '))
+
+if num1 < num2 and num1 < num3 :
+    if num2 < num3 :
+        print(f'El orden es : {num1},{num2},{num3}')
+    else :
+        print(f'El orden es : {num1},{num3},{num2}')
+
+elif num2 < num1 and num2 < num3 :
+    if num1 < num3 :
+        print(f'El orden es : {num2},{num1},{num3}')
+    else :
+        print(f'El orden es : {num2},{num3},{num1}')
+        
+else :
+    if num1 < num2 :
+        print(f'El orden es : {num3},{num1},{num2}')
+    else :
+        print(f'El orden es : {num3},{num2},{num1}')
+
+''''Empleo de ciclos en estructuras.'''
+
+# diccionario_contactos = {1 : 'Pedro', 2 : 'Marcos', 3 : 'Mauro', 4 : 'Maria'}
+# for k,v in diccionario_contactos.items() : 
+#     print(f'{k} -> {v}')
